505ebde3f32e1f41403343f71aa94892fe67974d
Md Istihad Tamim <109545844+IstihadTamim318@users.noreply.github.com>
2022-07-20 20:48:30 +0600

    Add files via upload

---
+++ Lab 01/318_49_08.cpp
@@ -0,0 +1,19 @@
+//codeforce problem no- 4A
+//Problem Name - Watermelon
+
+#include <iostream>
+using namespace std;
+
+int main()
+  {
+      int weight;
+  cin>>weight;
+
+  if(weight%2==0 && weight!=0)
+    cout<<"yes";
+  else
+    cout<<"No";
+  return 0;
+
+  }
+
